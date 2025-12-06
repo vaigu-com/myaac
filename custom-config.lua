2b09977a4be5fc3b81e9b67f694837eced4c61ea
un000000 <bolter156@gmail.com>
2025-12-06 04:00:44 +0000

    improve: include lua configs inside repo

---
+++ custom-config.lua
@@ -0,0 +1,33 @@
+ip = "vaigu.com"
+loginProtocolPort = 7171
+gameProtocolPort = 7172
+statusProtocolPort = 7171
+maxPlayers = 0
+serverName = "vaigu.com"
+serverMotd = "Welcome to the vaigu.com!"
+statusTimeout = 5 * 1000
+replaceKickOnLogin = true
+maxPacketsPerSecond = 35000
+maxItem = 2000
+maxContainer = 100
+maxPlayersOnlinePerAccount = 1000
+maxPlayersOutsidePZPerAccount = 1000
+
+ownerName = "vaigu.com"
+ownerEmail = "admin@vaigu.com"
+url = "https://vaigu.com/"
+location = "Europe"
+
+mysqlHost = "vaigu-db"
+mysqlUser = "otserver_db_user"
+mysqlPass = "57LUXxI6WD@TeVXi"
+mysqlDatabase = "otserver_db"
+
+toggleDownloadMap = false
+mapName = 'vaigu'
+
+kickIdlePlayerAfterMinutes = 1440
+saveIntervalTime = 10
+logLevel = "info"
+
+coinImagesURL = "https://vaigu.com/images/store/"

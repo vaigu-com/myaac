0d3b25295d84cb52c6866fe2888ed29a14be8096
un000000 <bolter156@gmail.com>
2025-12-31 00:12:55 +0100

    improve: rules and features page update

---
+++ system/pages/features.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Features
+ *
+ * @package   MyAAC
+ * @author    Gesior <jerzyskalski@wp.pl>
+ * @author    Slawkens <slawkens@gmail.com>
+ * @copyright 2019 MyAAC
+ * @link      https://my-aac.org
+ */
+defined('MYAAC') or die('Direct access not allowed!');
+$title = 'Features';
+
+$twig->display('features.html.twig', [
+	'page' => $_page,
+	'nextPage' => $nextPage,
+]);

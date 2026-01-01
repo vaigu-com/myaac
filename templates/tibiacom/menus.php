5885702d0e877b9968706655c31c7c14f1059c32
un000000 <bolter156@gmail.com>
2026-01-01 04:28:16 +0100

    fix: minor fixes

--- templates/tibiacom/menus.php
+++ templates/tibiacom/menus.php
@@ -4,7 +4,7 @@ return [
 	MENU_CATEGORY_NEWS => [
 		'Latest News' => 'news',
 		'News Archive' => 'news/archive',
-		'Changelog' => 'change-log',
+		#'Changelog' => 'change-log',
 	],
 	MENU_CATEGORY_ACCOUNT => [
 		'Account Management' => 'account/manage',
@@ -20,21 +20,21 @@ return [
 		'Last Kills' => 'last-kills',
 		'Houses' => 'houses',
 		'Guilds' => 'guilds',
-		'Polls' => 'polls',
-		'Bans' => 'bans',
+		#'Polls' => 'polls',
+		#'Bans' => 'bans',
 		'Support List' => 'team',
 	],
 	MENU_CATEGORY_FORUM => [
 		'Forum' => 'forum',
 	],
 	MENU_CATEGORY_LIBRARY => [
-		'Monsters' => 'monsters',
-		'Spells' => 'spells',
+		#'Monsters' => 'monsters',
+		#'Spells' => 'spells',
 		#'Commands' => 'commands',
 		'Exp Stages' => 'exp-stages',
-		'Gallery' => 'gallery',
+		#'Gallery' => 'gallery',
 		'Server Info' => 'ots-info',
-		'Exp Table' => 'exp-table',
+		#'Exp Table' => 'exp-table',
 		'FAQ' => 'faq',
 	],
 	MENU_CATEGORY_SHOP => [
